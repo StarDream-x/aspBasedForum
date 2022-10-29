416520a0015fa941a319df01fba85e5b911d7902
starDream <591121778@qq.com>
2022-10-29 23:32:46 +0800

    upload backend basic code

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# tsadmin
+后端部分 - asp.net web api 2  
---
+++ backend/TsAdm.Dashboard/RequestBodies/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TsAdm.Dashboard.RequestBodies
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string userId { get; set; }
+        public string content { get; set; }
+        public int likeCount { get; set; }
+        public int publishTime { get; set; }
+        public bool like { get; set; }
+    }
+}
