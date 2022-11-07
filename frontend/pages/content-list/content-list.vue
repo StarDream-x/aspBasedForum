4887ec8ba0092621378b1e21f87d6529257aea61
Tedddy <1669888132@qq.com>
2022-11-07 20:17:05 +0800

    update all methord, waiting for otherUser page

--- frontend/pages/content-list/content-list.vue
+++ frontend/pages/content-list/content-list.vue
@@ -1,36 +1,49 @@
 <template>
 	<view class="content-list">
+		<ContentListItem v-for="content in contents" :viewCount="content.viewCount" :author="content.authorName" :title="content.title" :imageSrc="content.imageUrl" @click="goToDetail(content)"/>
+		<!-- <ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
 		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
 		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
-		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
-		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
-		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" />
+		<ContentListItem :viewCount="114" author="作者" title="标题" imageSrc="/static/logo.png" /> -->
 	</view>
 </template>
 
 <script>
+	import {myRequest} from '~@/util/api.js'
 	import ContentListItem from '~@/components/content-list-item.vue'
 	export default {
 		data() {
 			return {
-				
+				contents:[]
 			}
 		},
 		components: {
 			ContentListItem
 		},
 		methods: {
-			getList(){
-				//TODO:获取用户收藏的文章列表
+			goToDetail(content){
+				uni.navigateTo({
+					url:"/pages/detail/detail",
+					success: (res) => {
+						getApp().globalData.toDetailContentId = content.id
+					}
+				})
 			}
 		},
-		onLoad: go(options){
-			this.event = this.getOpenerEventChannel()
-			//接收页面1传来的数据
-			this.event.on('id', (res) => {
-				this.userId = res
+		onLoad(options){
+			//TODO:获取用户收藏的文章列表
+			const userId = getApp().globalData.toContentListId
+			myRequest({
+				url: 'user/'+this.userId+"/favorite",
+				method: 'GET',
+				data: {
+					id: userId
+				}
+			}).then((res) => {
+				if (res.statusCode == 200){
+					this.contents = res.data
+				}
 			})
-			getList();
 		}
 	}
 </script>
